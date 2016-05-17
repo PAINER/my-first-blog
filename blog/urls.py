100d720929ff93c73c4e3c9314d99f5b7cda6212
PAINER <pit.mishazarov@gmail.com>
2016-05-17 21:37:34 +0300

    1/4

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
-from . import views
+from blog import views
+from django.conf.urls.static import static
 
-urlpatterns = [
+
+urlpatterns = patterns('',
     url(r'^$', views.post_list, name='post_list'),
-]
+    
+    url(r'^$', views.index, name='index'),
+
+    url(r'^$', views.edit, name='edit'),
+
+)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from blog.models import Post
+from django.http import HttpResponse
+from django.template import Context, loader
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+ 
+def index(request):
+    entries = BlogEntry.objects.all()
+    template = loader.get_template('blog/index.html')
+    context = Context({'entries': entries})
+    return HttpResponse(template.render(context))
 
+def edit(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/edit.html', {'questionName': edit})
