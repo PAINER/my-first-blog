4d5f4551dde024acafddc5227d5bbd74841af9fa
PAINER <pit.mishazarov@gmail.com>
2016-05-17 21:42:32 +0300

    Added views to create/edit blog post inside the site.aaa

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,11 @@ from blog import views
 from django.conf.urls.static import static
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     
     url(r'^$', views.index, name='index'),
 
     url(r'^$', views.edit, name='edit'),
 
-)
+]
